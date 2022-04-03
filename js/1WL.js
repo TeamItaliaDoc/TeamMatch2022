04a1cd8c0c6bb21f362f4c47f28dcbd1b587df9f
Sandro <sandro.torri@ellysse.it>
2022-04-03 08:28:57 +0200

    Prima versione

---
+++ js/1WL.js
@@ -0,0 +1,96 @@
+// 1WL-OPEN
+matchs['WL-OPEN-1'] = {"id":"1327015", "lega":"1WL-OPEN", "giornata":1, "data":"09/01/2022", "daCaricare" : true};
+matchs['WL-OPEN-2'] = {"id":"1339647", "lega":"1WL-OPEN", "giornata":2, "data":"13/02/2022", "daCaricare" : true};
+matchs['WL-OPEN-3'] = {"id":"1353861", "lega":"1WL-OPEN", "giornata":3, "data":"13/03/2022", "daCaricare" : true};
+matchs['WL-OPEN-4'] = {"id":"1362581", "lega":"1WL-OPEN", "giornata":4, "data":"10/04/2022", "daCaricare" : true};
+matchs['WL-OPEN-5'] = {"id":"", "lega":"1WL-OPEN", "giornata":5, "data":"08/05/2022", "daCaricare" : true};
+matchs['WL-OPEN-6'] = {"id":"", "lega":"1WL-OPEN", "giornata":6, "data":"12/06/2022", "daCaricare" : true};
+matchs['WL-OPEN-7'] = {"id":"", "lega":"1WL-OPEN", "giornata":7, "data":"10/07/2022", "daCaricare" : true};
+matchs['WL-OPEN-8'] = {"id":"", "lega":"1WL-OPEN", "giornata":8, "data":"14/08/2022", "daCaricare" : true};
+matchs['WL-OPEN-9'] = {"id":"", "lega":"1WL-OPEN", "giornata":9, "data":"11/08/2022", "daCaricare" : true};
+
+// 1WL-1800
+matchs['WL-1800-1'] = {"id":"1325663", "lega":"1WL-1800", "giornata":1, "data":"09/01/2022", "daCaricare" : true};
+matchs['WL-1800-2'] = {"id":"1339701", "lega":"1WL-1800", "giornata":2, "data":"13/02/2022", "daCaricare" : true};
+matchs['WL-1800-3'] = {"id":"0", "lega":"1WL-1800", "giornata":3, "data":"Da definire", "daCaricare" : true,
+"avversarioName":"Crimea team","risultato":"In attesa", "risultatoStyle" :"color:black", "giocatori":"", "score":"", "url" : ""};  //Vinto forfait
+matchs['WL-1800-4'] = {"id":"1362291", "lega":"1WL-1800", "giornata":4, "data":"10/04/2022", "daCaricare" : true};
+matchs['WL-1800-5'] = {"id":"", "lega":"1WL-1800", "giornata":5, "data":"08/05/2022", "daCaricare" : true};
+matchs['WL-1800-6'] = {"id":"", "lega":"1WL-1800", "giornata":6, "data":"12/06/2022", "daCaricare" : true};
+matchs['WL-1800-7'] = {"id":"", "lega":"1WL-1800", "giornata":7, "data":"10/07/2022", "daCaricare" : true};
+matchs['WL-1800-8'] = {"id":"", "lega":"1WL-1800", "giornata":8, "data":"14/08/2022", "daCaricare" : true};
+matchs['WL-1800-9'] = {"id":"", "lega":"1WL-1800", "giornata":9, "data":"11/08/2022", "daCaricare" : true};
+
+// 1WL-1600   
+matchs['WL-1600-1'] = {"id":"0", "lega":"1WL-1600", "giornata":1, "data":"09/01/2022", "daCaricare" : true,
+"avversarioName":"ChessAjedrezz","risultato":"Vinto forfait", "risultatoStyle" :"color:green;font-weight:bold", "giocatori":"", "score":"", "url" : ""};  //Vinto forfait
+matchs['WL-1600-2'] = {"id":"1339611", "lega":"1WL-1600", "giornata":2, "data":"13/02/2022", "daCaricare" : true};
+matchs['WL-1600-3'] = {"id":"1345805", "lega":"1WL-1600", "giornata":3, "data":"27/02/2022", "daCaricare" : true};
+matchs['WL-1600-4'] = {"id":"1353867", "lega":"1WL-1600", "giornata":4, "data":"13/03/2022", "daCaricare" : true};
+matchs['WL-1600-5'] = {"id":"1362811", "lega":"1WL-1600", "giornata":5, "data":"10/04/2022", "daCaricare" : true};
+matchs['WL-1600-6'] = {"id":"", "lega":"1WL-1600", "giornata":6, "data":"12/06/2022", "daCaricare" : true};
+matchs['WL-1600-7'] = {"id":"", "lega":"1WL-1600", "giornata":7, "data":"10/07/2022", "daCaricare" : true};
+matchs['WL-1600-8'] = {"id":"", "lega":"1WL-1600", "giornata":8, "data":"14/08/2022", "daCaricare" : true};
+matchs['WL-1600-9'] = {"id":"", "lega":"1WL-1600", "giornata":9, "data":"11/08/2022", "daCaricare" : true};
+
+// 1WL-1400
+matchs['WL-1400-1'] = {"id":"1325061", "lega":"1WL-1400", "giornata":1, "data":"09/01/2022", "daCaricare" : true};
+matchs['WL-1400-2'] = {"id":"1339613", "lega":"1WL-1400", "giornata":2, "data":"13/02/2022", "daCaricare" : true};
+matchs['WL-1400-3'] = {"id":"1351805", "lega":"1WL-1400", "giornata":3, "data":"13/03/2022", "daCaricare" : true};
+matchs['WL-1400-4'] = {"id":"1363385", "lega":"1WL-1400", "giornata":4, "data":"10/04/2022", "daCaricare" : true};
+matchs['WL-1400-5'] = {"id":"", "lega":"1WL-1400", "giornata":5, "data":"08/05/2022", "daCaricare" : true};
+matchs['WL-1400-6'] = {"id":"", "lega":"1WL-1400", "giornata":6, "data":"12/06/2022", "daCaricare" : true};
+matchs['WL-1400-7'] = {"id":"", "lega":"1WL-1400", "giornata":7, "data":"10/07/2022", "daCaricare" : true};
+matchs['WL-1400-8'] = {"id":"", "lega":"1WL-1400", "giornata":8, "data":"14/08/2022", "daCaricare" : true};
+matchs['WL-1400-9'] = {"id":"", "lega":"1WL-1400", "giornata":9, "data":"11/08/2022", "daCaricare" : true};
+
+// 1WL-1200
+matchs['WL-1200-1'] = {"id":"1325063", "lega":"1WL-1200", "giornata":1, "data":"09/01/2022", "daCaricare" : true};
+matchs['WL-1200-2'] = {"id":"1339615", "lega":"1WL-1200", "giornata":2, "data":"13/02/2022", "daCaricare" : true};
+matchs['WL-1200-3'] = {"id":"1351363", "lega":"1WL-1200", "giornata":3, "data":"13/03/2022", "daCaricare" : true};
+matchs['WL-1200-4'] = {"id":"1362391", "lega":"1WL-1200", "giornata":4, "data":"10/04/2022", "daCaricare" : true};
+matchs['WL-1200-5'] = {"id":"", "lega":"1WL-1200", "giornata":5, "data":"08/05/2022", "daCaricare" : true};
+matchs['WL-1200-6'] = {"id":"", "lega":"1WL-1200", "giornata":6, "data":"12/06/2022", "daCaricare" : true};
+matchs['WL-1200-7'] = {"id":"", "lega":"1WL-1200", "giornata":7, "data":"10/07/2022", "daCaricare" : true};
+matchs['WL-1200-8'] = {"id":"", "lega":"1WL-1200", "giornata":8, "data":"14/08/2022", "daCaricare" : true};
+matchs['WL-1200-9'] = {"id":"", "lega":"1WL-1200", "giornata":9, "data":"11/08/2022", "daCaricare" : true};
+
+// 1WL-960
+matchs['WL-960-1'] = {"id":"1320839", "lega":"1WL-960", "giornata":1, "data":"02/01/2022", "daCaricare" : true};
+matchs['WL-960-2'] = {"id":"1335981", "lega":"1WL-960", "giornata":2, "data":"06/02/2022", "daCaricare" : true};
+matchs['WL-960-3'] = {"id":"1348719", "lega":"1WL-960", "giornata":3, "data":"06/03/2022", "daCaricare" : true};
+matchs['WL-960-4'] = {"id":"1359679", "lega":"1WL-960", "giornata":4, "data":"03/04/2022", "daCaricare" : true};
+matchs['WL-960-5'] = {"id":"", "lega":"1WL-960", "giornata":5, "data":"/05/2022", "daCaricare" : true};
+matchs['WL-960-6'] = {"id":"", "lega":"1WL-960", "giornata":6, "data":"/06/2022", "daCaricare" : true};
+matchs['WL-960-7'] = {"id":"", "lega":"1WL-960", "giornata":7, "data":"/07/2022", "daCaricare" : true};
+matchs['WL-960-8'] = {"id":"", "lega":"1WL-960", "giornata":8, "data":"/08/2022", "daCaricare" : true};
+matchs['WL-960-9'] = {"id":"", "lega":"1WL-960", "giornata":9, "data":"1/08/2022", "daCaricare" : true};
+
+// OPEN-A
+matchs['WL-OPEN-A-1'] = {"id":"1346245", "lega":"1WL-OPEN-A", "giornata":1, "data":"27/02/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-2'] = {"id":"1357339", "lega":"1WL-OPEN-A", "giornata":2, "data":"27/03/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-3'] = {"id":"", "lega":"1WL-OPEN-A", "giornata":3, "data":"24/04/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-4'] = {"id":"", "lega":"1WL-OPEN-A", "giornata":4, "data":"29/05/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-5'] = {"id":"", "lega":"1WL-OPEN-A", "giornata":5, "data":"26/06/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-6'] = {"id":"", "lega":"1WL-OPEN-A", "giornata":6, "data":"31/07/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-7'] = {"id":"", "lega":"1WL-OPEN-A", "giornata":7, "data":"28/08/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-8'] = {"id":"", "lega":"1WL-OPEN-A", "giornata":8, "data":"25/09/2022", "daCaricare" : true};
+matchs['WL-OPEN-A-9'] = {"id":"", "lega":"1WL-OPEN-A", "giornata":9, "data":"30/10/2022", "daCaricare" : true};
+
+// OPEN-D5
+matchs['WL-OPEN-D5-1'] = {"id":"1346249", "lega":"1WL-OPEN-D5", "giornata":1, "data":"27/02/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-2'] = {"id":"1357427", "lega":"1WL-OPEN-D5", "giornata":2, "data":"27/03/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-3'] = {"id":"", "lega":"1WL-OPEN-D5", "giornata":3, "data":"24/04/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-4'] = {"id":"", "lega":"1WL-OPEN-D5", "giornata":4, "data":"29/05/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-5'] = {"id":"", "lega":"1WL-OPEN-D5", "giornata":5, "data":"26/06/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-6'] = {"id":"", "lega":"1WL-OPEN-D5", "giornata":6, "data":"31/07/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-7'] = {"id":"", "lega":"1WL-OPEN-D5", "giornata":7, "data":"28/08/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-8'] = {"id":"", "lega":"1WL-OPEN-D5", "giornata":8, "data":"25/09/2022", "daCaricare" : true};
+matchs['WL-OPEN-D5-9'] = {"id":"", "lega":"1WL-OPEN-D5", "giornata":9, "data":"30/10/2022", "daCaricare" : true};
+
+// Classi CUP 1700
+matchs['WL-CUP-1700-1-1'] = {"id":"0", "lega":"1WL-CUP-1700", "giornata":1, "data":"", "daCaricare" : true,
+"avversarioName":"greek-arena-tournoua-ellados","risultato":"Non disputata", "risultatoStyle" :"color:black;font-weight:bold", "giocatori":"", "score":"", "url" : ""};  //Vinto forfait
+matchs['WL-CUP-1700-1-2'] = {"id":"1342643", "lega":"1WL-CUP-1700", "giornata":2, "data":"20/02/2022", "daCaricare" : true};
+matchs['WL-CUP-1700-1-3'] = {"id":"1354367", "lega":"1WL-CUP-1700", "giornata":3, "data":"20/03/2022", "daCaricare" : true};
+
